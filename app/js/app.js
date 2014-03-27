e1deddb733336766fdc9898c0a8d6a9f1795fae7
Samuel Bengtsson <samuel.89.b@gmail.com>
2014-03-27 19:30:26 +0100

    Stripped down the klient side

--- app/js/app.js
+++ app/js/app.js
@@ -18,18 +18,6 @@ angular.module('myApp', [
 
   $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
 
-  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
-
-  $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'});
-
-  $routeProvider.when('/products', {templateUrl: 'partials/products.html', controller: 'ProductsCtrl'});
-
-  $routeProvider.when('/products/:brand', {templateUrl: 'partials/products.html', controller: 'ProductsCtrl'});
-
-  $routeProvider.when('/product/add', {templateUrl: 'partials/add_product.html', controller: 'ProductsCtrl'});
-
-  $routeProvider.when('/logout', {templateUrl: 'partials/login.html', controller: 'LogoutCtrl'});
-
   $routeProvider.otherwise({redirectTo: '/home'});
 
 }]);
@@ -40,10 +28,7 @@ angular.module('myApp', [
 angular.module('myApp').run(['$http', 'UserDataService', '$rootScope' , function($http, UserDataService, $rootScope){
 
     console.log('... Hello hello, I am starting... Look at me run!');
-    $http.get('../server/login').success(function(data){
-      UserDataService.setUser(data.user);
-      $rootScope.$broadcast('userLoggedIn');
-    })
+   
 
 }]);
 
