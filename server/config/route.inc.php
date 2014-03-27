26972334c53e78385d73fba68f5982d082e39d57
Johan <johanjobbfalk@hotmail.com>
2014-03-27 20:09:52 +0100

    added new route for register

--- server/config/route.inc.php
+++ server/config/route.inc.php
@@ -3,6 +3,7 @@
 // Example: wwww.url.com/home => Home@getPage
 return array(
 	'auth/login' => 'LoginCtrl@login',
+	'auth/register' => 'RegisterCtrl@register',
 	
 	// A default route is always nice! if something goes wrong
 	// example defaultRoute => ErrorCtrl@invalidRequest
