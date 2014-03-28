e76f1aaea261a5fc021217c0f8beebebcec53cfc
Samuel Bengtsson <samuel.89.b@gmail.com>
2014-03-28 15:45:03 +0100

    Fixed some bugs in the userService

--- app/js/services.js
+++ app/js/services.js
@@ -3,21 +3,24 @@
 /* Services */
 
 angular.module('myApp.services', [])
-
-  .service('UserService', ['$http', '$location' ,function($http, $location){
+  
+  //User Service, do anything that handle user information
+  .service('UserService', ['$http', '$location', '$rootScope', function($http, $location, $rootScope){
+    //Create a msg variable for any messages
     var msg = null;
+    //Create a username variable that hold the username later on
     var userName = null;
     return {
       //Register function
       register: function(userInput){
         //Do a http request to the server and send in userInput to server
         $http.post('../server/auth/register', userInput).success(function(data){
-          //console.log(data);
-          console.log(data);
+          //If the registration fails, then show the user a error message
           if(data['registerResponse'] == false){
+            //Set the error message here
             msg = 'Register error';
-            //console.log(t.msg);
           }else{
+            //If everything is okey with the registration, then show a complete messeage to the user
             msg = 'Registration complete, please sign in below'
           }
 
@@ -27,41 +30,33 @@ angular.module('myApp.services', [])
       login: function(loginInput){
         //Do a http request to the server and send in userInput to server
          $http.post('../server/auth/login', loginInput).success(function(data){
+          //If the login fails, then show a error message
           if(data['loginResponse'] == false){
+            //set the message here
             msg = 'A error with your login information';
-            console.log(msg);
           }else{
+            //if the login success, then set the username to the variable userName
             userName = data['loginResponse'];
-            console.log(userName)
+            //Start the menu event that change the menu
+            $rootScope.$broadcast('menuGet');
+            //Send the user to what page you want the user to land on after success login
             $location.path('/home');
           }
-          //console.log(data['loginResponse']);
         });
       },
 
+      //a function that returns the message that are set before
       getMsg: function(){
         return msg;
       },
+      //a function that returns the username if a login is success
       getUser: function(){
         return userName;
       }
     };
 
-  }])
-
-  .service('MenuService', ['$http', function($http){
-    return{
-      //var menuItem = null;
-      getMenu: function(){
-        $http.get('../server/menu/get').success(function(data){
-          //menuItem = data;
-          return data;
-        });
-      }
-
-    }
-
   }]);
+
 
 
   
