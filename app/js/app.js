81455a3b63533a98d2858cf9e16e039f2837fcc8
Samuel Bengtsson <samuel.89.b@gmail.com>
2014-04-04 14:11:06 +0200

    Change the rootprovider

--- app/js/app.js
+++ app/js/app.js
@@ -16,13 +16,34 @@ angular.module('myApp', [
 
 .config(['$routeProvider', function($routeProvider) {
 
-  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
-  $routeProvider.when('/login', {templateUrl: 'partials/login-register.html', controller: 'UserCtrl'});
-  $routeProvider.when('/categories', {templateUrl: 'partials/categories.html', controller: 'CategoryCtrl'});
-  $routeProvider.when('/logout', {templateUrl: 'partials/login-register.html', controller: 'LogoutCtrl'});
-  $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: 'ProfileCtrl'});
-  $routeProvider.when('/categories/:category', {templateUrl: 'partials/category_topic.html', controller: 'TopicCtrl'});
-  $routeProvider.when('/categories/:category/:topic', {templateUrl: 'partials/category_post.html', controller: 'PostCtrl'});
+  $routeProvider.when('/home',{
+      templateUrl: 'partials/home.html', 
+      controller: 'HomeCtrl'
+  })
+  .when('/login', {
+    templateUrl: 'partials/login-register.html', 
+    controller: 'UserCtrl'
+  })
+  .when('/categories', {
+    templateUrl: 'partials/categories.html', 
+    controller: 'CategoryCtrl'
+  })
+  .when('/logout', {
+    templateUrl: 'partials/login-register.html',
+    controller: 'LogoutCtrl'
+  })
+  .when('/profile', {
+    templateUrl: 'partials/profile.html', 
+    controller: 'ProfileCtrl'
+  })
+  .when('/categories/:category', {
+    templateUrl: 'partials/category_topic.html', 
+    controller: 'TopicCtrl'
+  })
+  .when('/categories/:category/:topic', {
+    templateUrl: 'partials/category_post.html', 
+    controller: 'PostCtrl'
+  });
 
   $routeProvider.otherwise({redirectTo: '/home'});
 
