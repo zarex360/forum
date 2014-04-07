e85ccec17f56b291db960db5b1503a0ea4c545c9
Johan <johanjobbfalk@hotmail.com>
2014-04-07 15:59:57 +0200

    changed query syntax

--- server/models/postModel.class.php
+++ server/models/postModel.class.php
@@ -20,40 +20,36 @@ class PostModel extends BaseModel
 
 	private function getPostsXtopic($tid)
 	{
-		$statement = $this->db->prepare(
-			"SELECT posts.* FROM posts 
+		$q = "SELECT posts.* FROM posts 
 			INNER JOIN posts_x_topic 
 			ON posts.id = posts_x_topic.pid
-			WHERE posts_x_topic.tid = :tid"
-		);
-		$statement->execute(array('tid' => $tid));
-		$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+			WHERE posts_x_topic.tid = :tid";
+
+		$r = array('tid' => $tid);
+		$result = $this->dbQuery($q, $r);
 		return $result;
 	}
 
 	private function getTopicForPost($cid, $tid)
 	{
-		$statement = $this->db->prepare(
-			"SELECT topics.* FROM topics
+		$q = "SELECT topics.* FROM topics
 			INNER JOIN topics_x_category
 			ON topics.id = topics_x_category.tid
 			INNER JOIN category_menu
 			ON category_menu.id = topics_x_category.cid
 			WHERE category_menu.id = :cid 
-			AND topics.id = :tid"
-		);
-		$statement->execute(array('cid' => $cid, 'tid' => $tid));
-		$result = $statement->fetch(PDO::FETCH_ASSOC);
+			AND topics.id = :tid";
+
+		$r = array('cid' => $cid, 'tid' => $tid);
+		$result = $this->dbQuery($q, $r, 'fetch');
 		return $result;
 	}
 
 	private function getCategoryId($href)
 	{
-		$statement = $this->db->prepare(
-			"SELECT id FROM category_menu WHERE href = :href"
-		);
-		$statement->execute(array('href' => $href));
-		$result = $statement->fetch(PDO::FETCH_ASSOC);
+		$q = "SELECT id FROM category_menu WHERE href = :href";
+		$r = array('href' => $href);
+		$result = $this->dbQuery($q, $r, 'fetch');
 		return $result['id'];
 	}
 }
